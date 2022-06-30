81a0983f62468e714939042cbbf7daef262d5585
Shravan Shandilya <shravan.shandilya@setu.co>
2022-06-30 09:21:29 +0530

    fix: form submit on pressing enter, fix: fixed page title

--- src/components/commentInput.jsx
+++ src/components/commentInput.jsx
@@ -8,6 +8,7 @@ function CommentInput({ parent, onUpdate }) {
   const [comment, setComment] = useState("");
 
   async function handleCommentButtonClick(event) {
+    event.preventDefault()
     await postComment(author.id, comment, parent);
     setComment("");
     if (onUpdate) onUpdate();
@@ -21,18 +22,20 @@ function CommentInput({ parent, onUpdate }) {
         src={author.display_picture}
         alt="author's profile"
       ></img>
-      <input
-        className="comment-input-input"
-        placeholder="What are your thoughts?"
-        onChange={(event) => setComment(event.target.value)}
-        value={comment || ""}
-      ></input>
-      <button
-        className="comment-input-button"
-        onClick={handleCommentButtonClick}
-      >
-        Comment
-      </button>
+      <form class="comment-input-form">
+        <input
+          className="comment-input-input"
+          placeholder="What are your thoughts?"
+          onChange={(event) => setComment(event.target.value)}
+          value={comment || ""}
+        ></input>
+        <input
+          type="submit"
+          className="comment-input-button"
+          onClick={handleCommentButtonClick}
+          value="Comment"
+        ></input>
+      </form>
     </div>
   );
 }
